fb17c984efa9b75d20d1cb47edc354dd02c3ee0e
Saikat1707 <berasaikat731@gmal.com>
2025-10-31 12:24:28 +0530

    correct the font

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"))
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173" || "https://lock-note-hazel.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials: true
 }))
